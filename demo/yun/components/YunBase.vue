5197e7ecaaffc17659acb2f48b14efb068e30954
YunYouJun <me@yunyoujun.cn>
2022-04-09 01:08:32 +0800

    docs: add i18n for i18n

--- demo/yun/components/YunBase.vue
+++ demo/yun/components/YunBase.vue
@@ -2,8 +2,6 @@
 import Base from 'valaxy-theme-yun/layouts/base.vue'
 
 // override theme base(global) layout
-
-const email = 'me@yunyoujun.cn'
 </script>
 
 <template>
@@ -34,15 +32,12 @@ const email = 'me@yunyoujun.cn'
       <slot name="right-sidebar" />
     </template>
     <template #right-custom>
-      <p p="t-4" font="serif black">
+      <hr m="10">
+      <p font="serif black">
         🌟 广告位招租 🌟
         <br>
         <del>云游君在线征婚</del>
       </p>
-      <a m="t-4" class="flex justify-center items-center" :href="`mailto:${email}`" :title="email" target="_blank">
-        <div class="inline-flex" m="r-2" i-ri-mail-line />
-        <span class="inline-flex">{{ email }}</span>
-      </a>
     </template>
   </Base>
 </template>
