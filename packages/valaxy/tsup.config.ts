792347bc87d8507bca0c704125c91d522b28773a
YunYouJun <me@yunyoujun.cn>
2022-05-13 01:52:45 +0800

    ci: add parallel build valaxy & theme

--- packages/valaxy/tsup.config.ts
+++ packages/valaxy/tsup.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig((options) => {
   return {
     entry: [
+      'src/index.ts',
       'src/node/index.ts',
       'src/node/cli.ts',
-      'src/index.ts',
     ],
     // https://tsup.egoist.sh/#code-splitting
     // Code splitting currently only works with the esm output format, and it's enabled by default. If you want code splitting for cjs output format as well, try using --splitting flag which is an experimental feature to get rid of the limitation in esbuild.
