2a995118c89f3f3c2d3db867d987a1e38c602db5
Rotten-LKZ <Rotten_LKZ@163.com>
2022-06-17 23:49:19 +0800

    fix: remove cover on post page

--- packages/valaxy-theme-yun/components/YunPageHeader.vue
+++ packages/valaxy-theme-yun/components/YunPageHeader.vue
@@ -3,19 +3,11 @@ defineProps<{
   color?: string
   icon?: string
   title?: string
-  cover?: string
 }>()
 </script>
 
 <template>
   <header class="post-header">
-    <img
-      v-if="cover"
-      :src="cover"
-      max-h="42vh"
-      min-h="260px"
-      class="select-none object-contain"
-    >
     <h1 class="post-title" p="2" text="2xl center" font="serif black" :style="`color:${color}`">
       <div v-if="icon" class="icon" m="r-1" inline-flex :class="icon" />
       <span>{{ title }}</span>
