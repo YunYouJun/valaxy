28d66b8474305f812077ce9718a0c6574e3b66b3
JeremyWuuuuu <15975785+JeremyWuuuuu@users.noreply.github.com>
2022-06-24 18:44:55 +0800

    Address PR comments

--- packages/valaxy-theme-yun/components/YunConfig.vue
+++ packages/valaxy-theme-yun/components/YunConfig.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
+import { computed } from 'vue'
 import { isDark, toggleDark } from '~/composables'
 
 const { t } = useI18n()
+
+const themeTitle = computed(() => {
+  return isDark.value ? t('button.toggle_light') : t('button.toggle_dark')
+})
 </script>
 
 <template>
   <div>
-    <button class="yun-icon-btn" :title="t('button.toggle_theme')" :style="{ color: isDark ? '' : '#f1cb64' }" @click="toggleDark()">
+    <button class="yun-icon-btn" :title="themeTitle" :style="{ color: isDark ? '' : '#f1cb64' }" @click="toggleDark()">
       <div i="ri-sun-line dark:ri-moon-line" />
     </button>
 
--- packages/valaxy-theme-yun/components/YunToggleLocale.vue
+++ packages/valaxy-theme-yun/components/YunToggleLocale.vue
@@ -19,7 +19,7 @@ const toggleLocales = () => {
 </script>
 
 <template>
-  <button class="yun-icon-btn" :title="t('button.change_lang')" style="color:var(--va-c-text)" @click="toggleLocales">
+  <button class="yun-icon-btn" :title="t('button.toggle_langs')" style="color:var(--va-c-text)" @click="toggleLocales">
     <div i-ri-translate class="transition transform" :class="locale === 'en' ? 'rotate-y-180' : ''" />
   </button>
 </template>
