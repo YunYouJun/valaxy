84e5bb3420abfc14d7029962dec7a017d9d54276
mrmao <wwu710632@gmail.com>
2022-08-02 11:31:23 +0800

    feat: add valaxy-addon-meting package

---
+++ packages/valaxy-addon-meting/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { useAplayer } from 'valaxy'
+import { computed } from 'vue'
+const props = defineProps<{
+  [key: string]: any
+}>()
+
+const loadAplayer = computed(() => props.auto || props.url || (props.id && props.server && props.type))
+
+if (loadAplayer.value)
+  useAplayer()
+</script>
+
+<template>
+  <meting-js
+    v-if="loadAplayer"
+    v-bind="props"
+    :fixed="true"
+  />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ packages/valaxy-addon-meting/README.md
@@ -0,0 +1,20 @@
+# valaxy-addon-meting
+
+Global meting music player
+
+```ts
+import { defineConfig } from 'valaxy'
+export default defineConfig({
+  addons: [
+    ['valaxy-addon-meting', {
+      props: {
+        /** @see https://github.com/metowolf/MetingJS */
+      }
+    }]
+  ]
+})
+```
+
+## props
+
+you can [meting#options](https://github.com/metowolf/MetingJS#option)
