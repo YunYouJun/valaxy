a0c7010950983a27464f754399ef48b3ebdf53ac
YunYouJun <me@yunyoujun.cn>
2023-01-25 23:37:08 +0800

    fix: docs build

--- packages/valaxy-addon-algolia/node/index.ts
+++ packages/valaxy-addon-algolia/node/index.ts
@@ -1,9 +1,8 @@
 import { defineValaxyAddon } from 'valaxy'
 import type { AlgoliaSearchOptions } from '../types'
-import pkg from '../package.json'
 
 export const addonAlgolia = defineValaxyAddon<AlgoliaSearchOptions>(options => ({
-  name: pkg.name,
+  name: 'valaxy-addon-algolia',
   enable: true,
   options,
 }))
