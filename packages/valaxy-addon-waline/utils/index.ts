a6aae6478639283a1e3a4d91560d55546f440aba
YunYouJun <me@yunyoujun.cn>
2023-04-22 20:16:24 +0800

    fix: custom emojis by types, close #211

--- packages/valaxy-addon-waline/utils/index.ts
+++ packages/valaxy-addon-waline/utils/index.ts
@@ -1,7 +1,9 @@
-export function getEmojis(cdn = '//unpkg.com/') {
-  return [
-    `${cdn}@waline/emojis/bilibili/`,
-    `${cdn}@waline/emojis/qq/`,
-    `${cdn}@waline/emojis/weibo/`,
-  ]
+/**
+ * Get emojis cdn path
+ * @param cdn
+ * @param types
+ * @returns
+ */
+export function getEmojis(cdn = '//unpkg.com/', types = ['bilibili', 'qq', 'weibo']) {
+  return types.map(type => `${cdn}@waline/emojis/${type}/`)
 }
