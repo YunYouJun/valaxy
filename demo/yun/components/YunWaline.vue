d95841f40d16e1d0d626931c39dd0c26763b92f3
YunYouJun <me@yunyoujun.cn>
2023-10-22 01:39:20 +0800

    release: v0.15.10

--- demo/yun/components/YunWaline.vue
+++ demo/yun/components/YunWaline.vue
@@ -1,30 +1,31 @@
 <script lang="ts" setup>
 import type { WalineInitOptions } from '@waline/client'
-import consola from 'consola'
+
+// import consola from 'consola'
 
 const customOptions: WalineInitOptions = {
   serverURL: 'https://waline.yunyoujun.cn',
   pageview: true,
   comment: true,
 
-  imageUploader: (file) => {
-    consola.info('Upload file: ', file)
+  // imageUploader: (file) => {
+  //   consola.info('Upload file: ', file)
 
-    const formData = new FormData()
-    const headers = new Headers()
+  //   const formData = new FormData()
+  //   const headers = new Headers()
 
-    formData.append('file', file)
-    headers.append('Authorization', '!{API TOKEN}')
-    headers.append('Accept', 'application/json')
+  //   formData.append('file', file)
+  //   headers.append('Authorization', '!{API TOKEN}')
+  //   headers.append('Accept', 'application/json')
 
-    return fetch('!{API URL}', {
-      method: 'POST',
-      headers,
-      body: formData,
-    })
-      .then(resp => resp.json())
-      .then(resp => resp.data.links.url)
-  },
+  //   return fetch('!{API URL}', {
+  //     method: 'POST',
+  //     headers,
+  //     body: formData,
+  //   })
+  //     .then(resp => resp.json())
+  //     .then(resp => resp.data.links.url)
+  // },
 }
 </script>
 
