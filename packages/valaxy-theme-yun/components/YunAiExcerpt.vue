13c0cff27094e065b5bed7644204073bc936df0d
Okysu <yby@ecanse.com>
2023-10-22 16:48:10 +0800

    perf: AI Summary Typing Style

--- packages/valaxy-theme-yun/components/YunAiExcerpt.vue
+++ packages/valaxy-theme-yun/components/YunAiExcerpt.vue
@@ -1,13 +1,38 @@
 <script lang="ts" setup>
 import { useFrontmatter } from 'valaxy'
 import { useI18n } from 'vue-i18n'
+import { ref, onMounted } from 'vue'
 
 const fm = useFrontmatter()
 const { t } = useI18n()
+
+const content = ref('')
+let timer: NodeJS.Timeout
+const typing = (interval = 80) => {
+  let i = 0
+  timer = setInterval(() => {
+    content.value += fm.value?.excerpt?.charAt(i)
+    i++
+    if (i === fm.value?.excerpt?.length) {
+      clearInterval(timer)
+    }
+  }, interval)
+  return timer
+}
+
+const clear = () => {
+  clearInterval(timer)
+  content.value = fm.value?.excerpt ?? ''
+}
+
+onMounted(() => {
+  typing()
+})
+
 </script>
 
 <template>
-  <div class="ai-generated-excerpt rounded-lg bg-$va-c-bg-alt p-4">
+  <div @click="clear" class="ai-generated-excerpt rounded-lg bg-$va-c-bg-alt p-4">
     <div font="black" flex items-center>
       <div mr-1 i-ri-robot-2-line />
       <div>
@@ -15,7 +40,7 @@ const { t } = useI18n()
       </div>
     </div>
     <div op="90" mt-1>
-      {{ fm.excerpt }}
+      {{ content }}
     </div>
   </div>
 </template>
