596a0ec834c74e0ff1c5291e975b6d59919a6cf4
Mr.Mao <951416545@qq.com>
2023-12-02 14:37:50 +0800

    fix: meting options fix type and field invalid

--- packages/valaxy-addon-meting/node/index.ts
+++ packages/valaxy-addon-meting/node/index.ts
@@ -2,12 +2,38 @@ import { defineValaxyAddon } from 'valaxy'
 import pkg from '../package.json'
 
 export interface MetingOptions {
-  global: boolean
+  global?: boolean
+  /**
+   * @see https://github.com/metowolf/MetingJS#option
+   */
+  options?: {
+    id?: string
+    server?: string
+    type?: string
+    auto?: string
+    fixed?: boolean
+    mini?: boolean
+    autoplay?: boolean
+    theme?: string
+    loop?: string
+    order?: string
+    preload?: string
+    volume?: number
+    work?: string
+    mutex?: boolean
+    start?: string
+    'lrc-type'?: number
+    'list-folded'?: boolean
+    'list-max-height'?: string
+    'storage-name'?: string
+    [key: string]: any
+  }
 }
+
 
 export const addonMeting = defineValaxyAddon<MetingOptions>(options => ({
   name: pkg.name,
   enable: true,
   global: options?.global ?? false,
-  options,
+  ...options,
 }))
