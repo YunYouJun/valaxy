863021c2e4b812cad5d200b23ce5b85c1852ae9e
YunYouJun <me@yunyoujun.cn>
2024-01-21 00:28:48 +0800

    test: add check for ssg dist draft

---
+++ test/build/demo.test.ts
@@ -0,0 +1,22 @@
+import { resolve } from 'node:path'
+import { describe, expect, it } from 'vitest'
+import fs from 'fs-extra'
+
+const demoFolder = resolve(__dirname, '../../demo')
+const demoYunDistFolder = resolve(demoFolder, 'yun/dist')
+
+// const mdDir = resolve(__dirname, 'fixtures/markdown')
+
+describe('check dist', async () => {
+  // hello-valaxy.md generate hello-valaxy.html
+  it('post should be generated', () => {
+    const helloValaxyHtml = resolve(demoYunDistFolder, 'posts/hello-valaxy.html')
+    expect(fs.existsSync(helloValaxyHtml)).toBe(true)
+  })
+
+  // draft.md not generate draft.html
+  it('draft should not be generated', () => {
+    const draftHtml = resolve(demoYunDistFolder, 'posts/draft.html')
+    expect(fs.existsSync(draftHtml)).toBe(false)
+  })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     deps: {
-      inline: ['@vue', '@vueuse', 'vue-demi'],
+      optimizer: {
+        ssr: {
+          include: ['@vue', '@vueuse', 'vue-demi'],
+        },
+      },
     },
   },
 })
