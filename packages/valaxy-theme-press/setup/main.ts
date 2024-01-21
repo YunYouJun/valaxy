74acc88321ae1e6d0a4483eb622296b096be3d7d
YunYouJun <me@yunyoujun.cn>
2024-01-21 20:22:20 +0800

    fix(theme-press): code highlight

--- packages/valaxy-theme-press/setup/main.ts
+++ packages/valaxy-theme-press/setup/main.ts
@@ -5,7 +5,7 @@ import 'vitepress/dist/client/theme-default/styles/vars.css'
 
 // import 'vitepress/dist/client/theme-default/styles/base.css'
 // import 'vitepress/dist/client/theme-default/styles/utils.css'
-// import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
+import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
 import 'vitepress/dist/client/theme-default/styles/components/vp-code-group.css'
 import 'vitepress/dist/client/theme-default/styles/components/vp-doc.css'
 import 'vitepress/dist/client/theme-default/styles/components/custom-block.css'
