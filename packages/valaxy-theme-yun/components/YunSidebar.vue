b9b82d45ed3f8229d74c467bcae5032a681c8ea9
YunYouJun <me@yunyoujun.cn>
2024-01-27 19:28:56 +0800

    fix(theme-yun): sidebar display in mobile

--- packages/valaxy-theme-yun/components/YunSidebar.vue
+++ packages/valaxy-theme-yun/components/YunSidebar.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { ref, watch } from 'vue'
-import { useAppStore, useLayout } from 'valaxy'
+import { ref } from 'vue'
+import { useAppStore } from 'valaxy'
 
 defineProps<{
   showHamburger?: boolean
@@ -8,13 +8,6 @@ defineProps<{
 
 const app = useAppStore()
 const showOverview = ref(false)
-
-const isHome = useLayout('home')
-watch(isHome, () => {
-  app.isSidebarOpen = !isHome.value
-}, {
-  immediate: true,
-})
 </script>
 
 <template>
