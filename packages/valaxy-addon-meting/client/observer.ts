4b2e10c9f7d5f478cb0377261d0bf8a956238138
WRXinYue <94453358+WRXinYue@users.noreply.github.com>
2024-02-25 03:53:02 +0800

    refactor: change onMounted and add onUnmounted for observer (#361)

--- packages/valaxy-addon-meting/client/observer.ts
+++ packages/valaxy-addon-meting/client/observer.ts
@@ -1,28 +1,30 @@
-import { onUnmounted } from 'vue'
+import { onMounted, onUnmounted } from 'vue'
 import type { MetingOptions } from '../node/index'
 import { onMetingLoad, onMetingLoadBefore } from './hook'
 
 export function setupHiddenLyricHidingObserver() {
   let observer: MutationObserver | null
 
-  observer = new MutationObserver((mutations) => {
-    const lrcElement = document.querySelector('.aplayer-lrc .aplayer-lrc-contents .aplayer-lrc-current') as HTMLElement
-    const lrcButton = document.querySelector('.aplayer-icon-lrc') as HTMLElement
-    function removelrc() {
-      if (lrcElement) {
-        lrcElement.style.display = 'none'
-        if (lrcElement.textContent !== 'Loading') {
-          lrcButton.click()
-          lrcElement.style.display = ''
-          observer?.disconnect()
+  onMounted(() => {
+    observer = new MutationObserver((mutations) => {
+      const lrcElement = document.querySelector('.aplayer-lrc .aplayer-lrc-contents .aplayer-lrc-current') as HTMLElement
+      const lrcButton = document.querySelector('.aplayer-icon-lrc') as HTMLElement
+      function removelrc() {
+        if (lrcElement) {
+          lrcElement.style.display = 'none'
+          if (lrcElement.textContent !== 'Loading') {
+            lrcButton.click()
+            lrcElement.style.display = ''
+            observer?.disconnect()
+          }
         }
       }
-    }
-    mutations.forEach((_mutation) => {
-      removelrc()
+      mutations.forEach((_mutation) => {
+        removelrc()
+      })
     })
+    observer.observe(document.body, { childList: true, subtree: true })
   })
-  observer.observe(document.body, { childList: true, subtree: true })
 
   onUnmounted(() => {
     observer?.disconnect()
@@ -34,28 +36,30 @@ export function useMetingLoadObserver(addon: MetingOptions) {
   let hasExecuted = false
   let observer: MutationObserver | null
 
-  observer = new MutationObserver((mutations) => {
-    function load() {
-      if (hasExecuted)
-        return
-      const aplayerNarrowElement = document.querySelector('.aplayer.aplayer-fixed.aplayer-narrow .aplayer-body') as HTMLElement
-      if (aplayerNarrowElement) {
-        hasExecuted = true
-        setTimeout(() => {
-          onMetingLoadBefore(addon)
-          requestAnimationFrame(() => {
-            onMetingLoad(addon)
-            observer?.disconnect()
-            observer = null
-          })
-        }, 0)
+  onMounted(() => {
+    observer = new MutationObserver((mutations) => {
+      function load() {
+        if (hasExecuted)
+          return
+        const aplayerNarrowElement = document.querySelector('.aplayer.aplayer-fixed.aplayer-narrow .aplayer-body') as HTMLElement
+        if (aplayerNarrowElement) {
+          hasExecuted = true
+          setTimeout(() => {
+            onMetingLoadBefore(addon)
+            requestAnimationFrame(() => {
+              onMetingLoad(addon)
+              observer?.disconnect()
+              observer = null
+            })
+          }, 0)
+        }
       }
-    }
-    mutations.forEach((_mutation) => {
-      load()
+      mutations.forEach((_mutation) => {
+        load()
+      })
     })
+    observer.observe(document.body, { childList: true, subtree: true })
   })
-  observer.observe(document.body, { childList: true, subtree: true })
 
   onUnmounted(() => {
     observer?.disconnect()
