755bcd464c987c5c55c618cb89b675b1c8766397
krau <krau39@outlook.com>
2024-03-27 23:59:19 +0800

    feat(theme-yun): support artalk comment system

---
+++ packages/valaxy-theme-yun/components/third/YunArtalk.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import * as addonArtalk from 'valaxy-addon-artalk'
+import 'valaxy-addon-artalk/client/styles/index.scss'
+import { isEmptyAddon } from 'valaxy'
+
+if (!isEmptyAddon(addonArtalk))
+  addonArtalk.useArtalkWithOptions()
+</script>
+
+<template>
+  <ArtalkClient />
+</template>
