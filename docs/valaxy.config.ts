3932b6e29c508b5096ca3c72b8de808790f5a5bb
YunYouJun <me@yunyoujun.cn>
2024-06-16 20:23:28 +0800

    docs: update title style

--- docs/valaxy.config.ts
+++ docs/valaxy.config.ts
@@ -15,7 +15,7 @@ const safelist = [
 
 export default defineValaxyConfig<PressTheme.Config>({
   siteConfig: {
-    title: 'Valaxy',
+    title: 'VALAXY',
     url: 'https://valaxy.site',
     description: 'Valaxy Site Docs',
 
