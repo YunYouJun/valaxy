3ceab6d4c6a674ed15135e066d004a295b5e65d1
BlueGlassBlock <blueglassblock@outlook.com>
2024-07-07 02:00:18 +0800

    chore: try fix tests x3

--- test/client/frontmatter.test.ts
+++ test/client/frontmatter.test.ts
@@ -1,6 +1,6 @@
 // import { resolve } from 'path'
 import { describe, expect, it } from 'vitest'
-import { format, fromZonedTime } from 'date-fns-tz'
+import { formatInTimeZone, fromZonedTime } from 'date-fns-tz'
 
 const cnTimezone = 'Asia/Shanghai'
 // const mdDir = resolve(__dirname, 'fixtures/markdown')
@@ -11,7 +11,7 @@ describe('frontmatter parse', async () => {
     const utcDate = fromZonedTime('2023-07-19 10:55:53Z', cnTimezone)
     const timestampDate = fromZonedTime(+date, cnTimezone)
 
-    const formattedDate = format(date, 'yyyy-MM-dd HH:mm:ss', { timeZone: cnTimezone })
+    const formattedDate = formatInTimeZone(date, cnTimezone, 'yyyy-MM-dd HH:mm:ss')
     expect(formattedDate).toEqual('2023-07-19 18:55:53')
 
     expect(date).toEqual(utcDate)
@@ -23,7 +23,7 @@ describe('frontmatter parse', async () => {
     const utcDate = fromZonedTime('2023-07-19T00:00:00+08:00', cnTimezone)
     const timestampDate = fromZonedTime(+date, cnTimezone)
 
-    const formattedDate = format(date, 'yyyy-MM-dd HH:mm:ss', { timeZone: cnTimezone })
+    const formattedDate = formatInTimeZone(date, cnTimezone, 'yyyy-MM-dd HH:mm:ss')
     expect(formattedDate).toEqual('2023-07-19 00:00:00')
 
     expect(date).toEqual(utcDate)
