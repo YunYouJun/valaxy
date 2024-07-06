c00332d3f645bb5a6b9ec95b53776735f7f9222f
BlueGlassBlock <blueglassblock@outlook.com>
2024-07-07 01:44:14 +0800

    chore: try fix tests

--- test/client/frontmatter.test.ts
+++ test/client/frontmatter.test.ts
@@ -1,6 +1,6 @@
 // import { resolve } from 'path'
 import { describe, expect, it } from 'vitest'
-import { format } from 'date-fns'
+import { formatISO } from 'date-fns'
 import { fromZonedTime } from 'date-fns-tz'
 
 const cnTimezone = 'Asia/Shanghai'
@@ -12,8 +12,8 @@ describe('frontmatter parse', async () => {
     const utcDate = fromZonedTime('2023-07-19 10:55:53Z', cnTimezone)
     const timestampDate = fromZonedTime(+date, cnTimezone)
 
-    const formattedDate = format(date, 'yyyy-MM-dd HH:mm:ss')
-    expect(formattedDate).toBe('2023-07-19 18:55:53')
+    const formattedDate = formatISO(date)
+    expect(formattedDate).toEqual('2023-07-19T18:55:53+08:00')
 
     expect(date).toEqual(utcDate)
     expect(date).toEqual(timestampDate)
@@ -24,8 +24,8 @@ describe('frontmatter parse', async () => {
     const utcDate = fromZonedTime('2023-07-19T00:00:00+08:00', cnTimezone)
     const timestampDate = fromZonedTime(+date, cnTimezone)
 
-    const formattedDate = format(date, 'yyyy-MM-dd')
-    expect(formattedDate).toBe('2023-07-19')
+    const formattedDate = formatISO(date)
+    expect(formattedDate).toEqual('2023-07-19T00:00:00+08:00')
 
     expect(date).toEqual(utcDate)
     expect(date).toEqual(timestampDate)
