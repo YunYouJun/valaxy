33af0706580cf577de8d4b109de84a8b60a3544d
YunYouJun <me@yunyoujun.cn>
2024-07-09 12:13:23 +0800

    docs: fix banner width

--- packages/valaxy-theme-press/components/PressHomeHero.vue
+++ packages/valaxy-theme-press/components/PressHomeHero.vue
@@ -16,20 +16,16 @@ const { t } = useI18n()
     </h1>
   </div>
 
-  <h2 px="2" m="b-10" text="center 6xl" font="black" leading="tight">
-    <span>{{ t('banner.next-generation') }}</span>
-    <br>
-    <span>{{ t('banner.static') }} </span>
-    <span class="gradient-text from-blue-500 to-purple-700" bg="gradient-to-r">{{ t('banner.blog') }}</span>
-    <span class="break-keep">{{ t('banner.framework') }}</span>
+  <h2 flex="~ wrap justify-center" px="2" m="b-10" text="center 6xl" font="black" leading="tight">
+    <span mx-1>{{ t('banner.next-generation') }}</span>
+    <span mx-1>{{ t('banner.static') }} </span>
+    <span mx-1 class="gradient-text from-blue-500 to-purple-700" bg="gradient-to-r">{{ t('banner.blog') }}</span>
+    <span mx-1 class="break-keep">{{ t('banner.framework') }}</span>
   </h2>
 
   <div p="2" text="center">
     <PressButton
-      v-for="action in fm.hero.actions"
-      :key="action.link"
-      :theme="action.theme"
-      :link="action.link"
+      v-for="action in fm.hero.actions" :key="action.link" :theme="action.theme" :link="action.link"
       :text="action.text"
     />
   </div>
