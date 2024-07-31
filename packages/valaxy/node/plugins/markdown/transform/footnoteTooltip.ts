9e279b2193c3db95b98402edfafa6c0f540af5ad
BlueGlassBlock <blueglassblock@outlook.com>
2024-07-31 12:12:24 +0800

    chore: cleanup debug

--- packages/valaxy/node/plugins/markdown/transform/footnoteTooltip.ts
+++ packages/valaxy/node/plugins/markdown/transform/footnoteTooltip.ts
@@ -1,7 +1,3 @@
-import debug from 'debug'
-
-const d = debug('valaxy:md')
-
 /**
  * Transforms fake elements ValaxyFootnoteRef, ValaxyFootnoteItem, ValaxyFootnoteAnchor to actual elements
  */
@@ -9,13 +5,10 @@ export function transformFootnoteTooltip(code: string) {
   const footnoteContentMap = new Map<string, string>()
   return code.replace(/<ValaxyFootnoteItem id="(.*?)">(.*?)<\/ValaxyFootnoteItem>/gs, (_, id: string, content: string) => {
     // Strip out ValaxyFootnoteAnchor
-    d('content', content)
     const tooltipContent = content.match(/<ValaxyFootnoteContent>(.*?)<\/ValaxyFootnoteContent>/s)![1].replace(/<ValaxyFootnoteAnchor>(.*?)<\/ValaxyFootnoteAnchor>/s, '')
     const itemContent = content
       .replace(/<ValaxyFootnoteContent>(.*?)<\/ValaxyFootnoteContent>/s, (_, content) => content)
       .replace(/<ValaxyFootnoteAnchor>(.*?)<\/ValaxyFootnoteAnchor>/s, (_, anchor) => anchor)
-    d('tooltipContent', tooltipContent)
-    d('itemContent', itemContent)
     footnoteContentMap.set(id, tooltipContent)
     return itemContent
   }).replace(/<ValaxyFootnoteRef href="#(.*?)">(.*?)<\/ValaxyFootnoteRef>/gs, (_, href: string, content: string) => {
