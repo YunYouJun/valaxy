062451ada9d1b84e1ed835a7b1f202d0cb1c03ea
YunYouJun <me@yunyoujun.cn>
2024-12-07 22:49:12 +0800

    test(e2e): add scroll into view for outline

---
+++ e2e/theme-yun/outline.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test'
+import { env } from '../env'
+
+test.use({
+  baseURL: env['theme-yun'],
+})
+
+test.describe('outline', () => {
+  test('long toc scroll', async ({ page }) => {
+    await page.goto('/posts/long-toc')
+
+    await page.waitForSelector('.markdown-body')
+    await expect(page.locator('.yun-aside')).toBeInViewport()
+
+    // scroll
+    await page.evaluate(() => {
+      window.scrollTo(0, 10000)
+    })
+    await expect(page.locator('.va-toc .va-toc-item').first()).not.toBeInViewport()
+    await expect(page.locator('.va-toc .va-toc-item').last()).toBeInViewport()
+  })
+})
