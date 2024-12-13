cc67e0ee05be96c374171b03dea319384c9b2e20
YunYouJun <me@yunyoujun.cn>
2024-12-13 23:53:49 +0800

    fix(theme-yun): nav menu link when absolute url

--- packages/valaxy-theme-yun/components/menu/YunNavMenuItem.vue
+++ packages/valaxy-theme-yun/components/menu/YunNavMenuItem.vue
@@ -1,22 +1,17 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-
 defineProps<{
   icon: string
   to?: string
 }>()
-
-const router = useRouter()
 </script>
 
 <template>
-  <div
-    class="size-12 inline-flex-center cursor-pointer z-$yun-z-nav-menu"
-    text="$va-c-text"
+  <AppLink
+    class="size-12 inline-flex-center cursor-pointer z-$yun-z-nav-menu text-$va-c-text"
     hover="bg-white/80 dark:bg-black/80 op-100"
     op-80
-    @click="to && router.push(to)"
+    :to="to"
   >
     <div class="size-6" :class="icon" />
-  </div>
+  </AppLink>
 </template>
