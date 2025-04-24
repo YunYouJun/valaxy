e9106602ae3017b9926d64b9e6fcdd8c8aab41f0
YunYouJun <me@yunyoujun.cn>
2025-04-24 19:45:50 +0800

    fix: abbrlink alias build when beforeWriteFiles

--- packages/valaxy-addon-abbrlink/node/index.ts
+++ packages/valaxy-addon-abbrlink/node/index.ts
@@ -1,5 +1,4 @@
 import type { Options } from '../types'
-import path from 'node:path'
 import { defineValaxyAddon } from 'valaxy'
 
 import pkg from '../package.json'
@@ -11,30 +10,30 @@ export const addonAbbrlink = defineValaxyAddon<Options>(options => ({
 
   setup(node) {
     node.hook('vue-router:extendRoute', (route) => {
-      if (route.meta.frontmatter.abbrlink) {
+      if (route.meta.frontmatter?.abbrlink) {
         route.addAlias(`/posts/${route.meta.frontmatter.abbrlink}`)
       }
     })
 
-    node.hook('vue-router:beforeWriteFiles', (root) => {
-      const postsRoot = root.children.find(child => child.path === '/posts')
-      if (!postsRoot)
-        return
+    // node.hook('vue-router:beforeWriteFiles', (root) => {
+    //   const postsRoot = root.children.find(child => child.path === '/posts')
+    //   if (!postsRoot)
+    //     return
 
-      const abbrlinkChildren = postsRoot.children.filter(child => child.meta.frontmatter?.abbrlink)
+    //   const abbrlinkChildren = postsRoot.children.filter(child => child.meta.frontmatter?.abbrlink)
 
-      if (abbrlinkChildren.length > 0) {
-        abbrlinkChildren.forEach((child) => {
-          const { abbrlink } = child.meta.frontmatter
-          if (abbrlink) {
-            postsRoot.insert(abbrlink, path.join(node.options.userRoot, 'pages/posts', `${child.path}.md`))
+    //   if (abbrlinkChildren.length > 0) {
+    //     abbrlinkChildren.forEach((child) => {
+    //       const { abbrlink } = child.meta.frontmatter
+    //       if (abbrlink) {
+    //         postsRoot.insert(abbrlink, path.join(node.options.userRoot, 'pages/posts', `${child.path}.md`))
 
-            if (options?.override) {
-              postsRoot.children.find(c => c.path === child.path)?.delete()
-            }
-          }
-        })
-      }
-    })
+    //         if (options?.override) {
+    //           postsRoot.children.find(c => c.path === child.path)?.delete()
+    //         }
+    //       }
+    //     })
+    //   }
+    // })
   },
 }))
