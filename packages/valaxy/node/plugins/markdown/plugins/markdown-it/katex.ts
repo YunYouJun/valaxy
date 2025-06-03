a8a6f346043a6501210d18c52bf36de11524056d
初五 <141227996+ChuwuYo@users.noreply.github.com>
2025-06-03 18:39:08 +0800

    fix(markdown): KaTeX Error Handling Issue

--- packages/valaxy/node/plugins/markdown/plugins/markdown-it/katex.ts
+++ packages/valaxy/node/plugins/markdown/plugins/markdown-it/katex.ts
@@ -177,9 +177,10 @@ export default function math_plugin(md: any, options: KatexOptions) {
       return katex.renderToString(latex, options)
     }
     catch (error) {
-      if (options.throwOnError)
-
-        console.warn(error)
+      if (options.throwOnError) {  
+        throw error  
+      } 
+      console.warn(error)
       return latex
     }
   }
@@ -194,8 +195,9 @@ export default function math_plugin(md: any, options: KatexOptions) {
       return `<p>${katex.renderToString(latex, options)}</p>`
     }
     catch (error) {
-      if (options.throwOnError)
-
+      if (options.throwOnError) {  
+        throw error  
+      } 
         console.warn(error)
       return latex
     }
