6548b37fcb37cd9dae741c34d09e094405028195
YunYouJun <me@yunyoujun.cn>
2025-07-21 21:44:08 +0800

    feat: add valaxy debug cmd console

--- packages/valaxy/node/cli/debug.ts
+++ packages/valaxy/node/cli/debug.ts
@@ -1,11 +1,18 @@
 import type { Argv } from 'yargs'
+import os from 'node:os'
+import process from 'node:process'
 import { consola } from 'consola'
+import { colors } from 'consola/utils'
+import { version } from '../../package.json'
 
 /**
  * @TODO valaxy debug
  */
 export function registerDebugCommand(cli: Argv) {
   cli.command('debug', 'Debug your blog', async () => {
-    consola.info('提问前请携带以下信息：')
+    console.log()
+    consola.log('  Operating System:', colors.green(os.platform()))
+    consola.log('  Node.JS Version:', colors.green(process.version))
+    consola.log('  Valaxy Version:', colors.cyan(`v${version}`))
   })
 }
