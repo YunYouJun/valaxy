55c3c98dce8fe11f3ce7189a8b53df9a3c1c6d39
RotCool <Rotten_LKZ@163.com>
2025-07-27 16:11:20 +0800

    fix(devtools): backslash in windows not supported by fast-glob (#594)
    * fix(devtools): backslash in windows not supported by fast-glob
    
    * fix(devtools): use pathe to resole slash problem in windows

--- packages/devtools/src/node/functions.ts
+++ packages/devtools/src/node/functions.ts
@@ -1,27 +1,26 @@
 import type { ViteDevServer } from 'vite'
 import type { ServerFunctions } from '../../rpc'
 import type { ValaxyDevtoolsOptions } from './types'
-import path from 'node:path'
 import process from 'node:process'
 import dayjs from 'dayjs'
 import fg from 'fast-glob'
 import fs from 'fs-extra'
 import matter from 'gray-matter'
+import pathe from 'pathe'
 
 function ensurePrefix(prefix: string, str: string) {
   if (!str.startsWith(prefix))
     return prefix + str
-  return str
 }
 
 export function getFunctions(server: ViteDevServer, devtoolsOptions: ValaxyDevtoolsOptions): ServerFunctions {
-  const userRoot = devtoolsOptions.userRoot || process.cwd()
+  const userRoot = (devtoolsOptions.userRoot || process.cwd()).replace(/\\/g, '/')
   // const userRoot = GLOBAL_STATE.valaxyApp?.options.userRoot || process.cwd()
   // const userRoot = process.cwd()
   // const userRoot = server.config.root
 
   function getRoutePath(filePath: string) {
-    const relativePath = path.relative(path.resolve(userRoot, 'pages'), filePath).slice(0, -'.md'.length)
+    const relativePath = pathe.relative(pathe.resolve(userRoot, 'pages'), filePath).slice(0, -'.md'.length)
     return ensurePrefix('/', relativePath)
   }
 
@@ -59,7 +58,7 @@ export function getFunctions(server: ViteDevServer, devtoolsOptions: ValaxyDevto
 
     async getPageData(pagePath: string) {
       const relativePath = pagePath.startsWith('/') ? pagePath.slice(1) : pagePath
-      const file = path.resolve(userRoot, relativePath)
+      const file = pathe.resolve(userRoot, relativePath)
       const { data } = matter(file)
 
       return {
