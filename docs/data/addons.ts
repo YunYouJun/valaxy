302d89e0f427d0a942700dd35377511fddf7acf8
YunYouJun <me@yunyoujun.cn>
2026-09-20 23:48:00 +0800

    refactor(utils): extract shared addon catalog constants

--- docs/data/addons.ts
+++ docs/data/addons.ts
@@ -1,3 +1,3 @@
-// Shared with the DevTools addon marketplace.
+// Shared with DevTools; edit packages/@valaxyjs/utils/src/constants/addons.ts.
 export { addons, getAddonDocsPath, localizeAddon, normalizeAddonLocale, officialAddons } from '@valaxyjs/utils'
 export type { AddonKind, AddonLocale, LocalizedAddonText, LocalizedValaxyAddon, AddonCatalogEntry as ValaxyAddon } from '@valaxyjs/utils'
